d35b9921207e30e6afe07cece08a7cbb4605758d
Crowdbotics <team@crowdbotics.com>
2023-08-13 17:09:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pet-sim-x-store-42827",
+  url: "https://pet-sim-x-store-42827.botics.co",
+  api: "https://pet-sim-x-store-42827.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
